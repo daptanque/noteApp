4a77b5e2ae6cc2dc40481d462a73f4c4fd788ddc
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-01-22 16:05:15 +0000

    Create Note entity add Parcelize to graddle

---
+++ app/src/main/java/com/example/notetakingapp/model/Note.kt
@@ -0,0 +1,17 @@
+package com.example.notetakingapp.model
+
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "notes")
+@Parcelize
+data class Note(
+    @PrimaryKey
+    val id : Int,
+    val noteTitle : String,
+    val noteBody : String
+):Parcelable
