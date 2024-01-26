77af465910d87700dee1b693889c121a026421de
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-01-26 15:15:48 +0000

    Set the HomeFragment
    Display the notes, enable searchView, using binding and live data

--- app/src/main/java/com/example/notetakingapp/MainActivity.kt
+++ app/src/main/java/com/example/notetakingapp/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.notetakingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notetakingapp.viewmodel.NoteViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
