749569e66c2cc8ed2a318dfdd809c3f184f131a2
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-01-28 13:17:12 +0000

    Update Note Fragment

--- app/src/main/java/com/example/notetakingapp/fragments/UpdateNoteFragment.kt
+++ app/src/main/java/com/example/notetakingapp/fragments/UpdateNoteFragment.kt
@@ -5,17 +5,75 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.notetakingapp.MainActivity
 import com.example.notetakingapp.R
+import com.example.notetakingapp.adapter.NoteAdapter
+import com.example.notetakingapp.databinding.FragmentHomeBinding
+import com.example.notetakingapp.databinding.FragmentNewNoteBinding
+import com.example.notetakingapp.databinding.FragmentUpdateNoteBinding
+import com.example.notetakingapp.model.Note
+import com.example.notetakingapp.viewmodel.NoteViewModel
 
-class UpdateNoteFragment : Fragment() {
+class UpdateNoteFragment : Fragment(R.layout.fragment_update_note) {
 
+    private var _binding: FragmentUpdateNoteBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var notesViewModel: NoteViewModel
+    private lateinit var currentNote : Note
+
+    private val args: UpdateNoteFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_note, container, false)
+        _binding = FragmentUpdateNoteBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        notesViewModel = (activity as MainActivity).noteViewModel
+        currentNote = args.note!!
+
+        binding.etNoteTitleUpdate.setText(currentNote.noteTitle)
+        binding.etNoteBodyUpdate.setText(currentNote.noteBody)
+
+
+        binding.fabDone.setOnClickListener{
+            val title = binding.etNoteTitleUpdate.text.toString().trim()
+            val body = binding.etNoteBodyUpdate.text.toString().trim()
+
+            if(title.isNotEmpty()){
+                val note = Note(currentNote.id,  currentNote.noteTitle, currentNote.noteBody)
+                notesViewModel.updateNote(note)
+                view.findNavController().navigate(R.id.action_updateNoteFragment_to_homeFragment)
+            }else{
+                Toast.makeText(context, "Please enter note Title", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+
+
+    private fun deleteNote(){
+
+    }
 }
