e8a84e7b03b8fbb9a32c77a322bbc0f2ac6b614b
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-01-28 13:25:46 +0000

    Finalizing with AlertDialog to Delete Note

--- app/src/main/java/com/example/notetakingapp/fragments/UpdateNoteFragment.kt
+++ app/src/main/java/com/example/notetakingapp/fragments/UpdateNoteFragment.kt
@@ -3,9 +3,13 @@ package com.example.notetakingapp.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.notetakingapp.MainActivity
@@ -74,6 +78,40 @@ class UpdateNoteFragment : Fragment(R.layout.fragment_update_note) {
 
 
     private fun deleteNote(){
+        activity?.let {
+            AlertDialog.Builder(it).apply{
+                setTitle("Delete Note")
+                setMessage("Are you sure you want to delete Note?")
+                setPositiveButton("Delete"){
+                    _,_ ->
+                    notesViewModel.deleteNote(currentNote)
 
+                    view?.findNavController()?.navigate(
+                        R.id.action_updateNoteFragment_to_homeFragment
+                    )
+                }
+                setNegativeButton("Cancel", null)
+            }.create().show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.menu_update_note, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_delete -> {
+                deleteNote()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
